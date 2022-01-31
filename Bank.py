295fb67da267c1f5f271366ad96a70588ad18cc2
pirtyraven <89965279+pirtyraven@users.noreply.github.com>
2022-01-31 12:11:19 +0100

    Nya småfix, tagit bort kommentarer osv.

--- Bank.py
+++ Bank.py
@@ -29,6 +29,7 @@ class Bank():
         return self.customer_data
 
     def load_customers(self):
+        #Bank._load(self)
         for i in self.customer_data:
             y = i.replace("#", ":").split(":")
             cstmr = Customer(y[0], y[1], y[2])
@@ -37,14 +38,16 @@ class Bank():
         return self.customers
 
     def get_customers(self):
-
+        #Bank.load_customers(self)
         for cstmr in self.customers:
             print(f"Name: {cstmr.name}, Social security number: {cstmr.pnr}")
 
     def get_customer(self, pnr):
-
+        #Bank._load(self)
+        #Bank.load_customers(self)
+        #Bank.load_accounts(self)
         if re.match('[0-9]{6}-[0-9]{4}', pnr) is None:
-            print("Sorry wrong format, please enter personal number as xxxxxx-xxxx")
+            print("\nSorry wrong format, please enter social security number as xxxxxx-xxxx")
             return False
 
         for i in self.customers:
@@ -56,29 +59,8 @@ class Bank():
 
                     if i.id in line.id:
                         the_accounts += f"Account number: {line.accountno}, Account type: {line.accounttype}, Balance: {line.balance}\n"
+                print(f"\n{the_customer}\n\nAccounts: \n{the_accounts}")
 
-        print(f"{the_customer}\n\nAccounts: \n{the_accounts}")
-
-    # def get_customer(self, pnr):
-    #
-    #     for i in self.customers:
-    #         if pnr == i.pnr:
-    #             x = str(i).split()
-    #             y = x[0], x[1], x[2], x[3]
-    #             id = y[0]
-    #             for line in self.accounts:
-    #                 if i.id in line.id:
-    #                         self.acc.append(line)
-    #                         z = re.sub(",", ":", str(self.acc))
-    #                         t = str(z).strip().split(":")
-    #                         if len(t) > 3:
-    #                             firstacc = (f"First account:\n\tAccount no: {t[0]}\n\tAccount type: {t[1]}\n\tBalance: {t[2]}\n")
-    #                             secondacc = (f"Second account: \n\tAccount no: {t[3]}\n\tAccount type: {t[4]}\n\tBalance: {t[5]}")
-    #                         elif len(t) == 3:
-    #                             firstacc = (f"First account:\n\tAccount no: {t[0]}\n\tAccount type: {t[1]}\n\tBalance: {t[2]}\n")
-    #                             secondacc = (f"Second account: \n\tAccount no: {None}\n\tAccount type: {None}\n\tBalance: {None}")
-    #
-    #     print(f"Customer id: {y[0]}\nName: {y[1]} {y[2]}\nSocial security number: {y[3]}\n{firstacc}{secondacc}")
     def load_accounts(self):
         self.accounts = []
         account_data = {}
@@ -94,35 +76,29 @@ class Bank():
                 self.accountNo.append(the_account.accountno)
                 nr_acc -= 1
 
-        #print(self.accountNo)
-        #print(self.accounts)
-
-
     def get_accounts(self):
         Bank.load_accounts(self)
         self.acc_list = []
 
         for i in self.accounts:
             account = f"{i.id} {i.accountno} {i.accounttype} {i.balance}"
-            #account = str(i.id) + " " + str(i.accountno) + " " + i.accounttype + " " + str(i.balance)
             self.acc_list.append(account)
-        #print(self.acc_list)
 
     def get_account(self, pnr, acc_no):
         if re.match('[0-9]{6}-[0-9]{4}', pnr) is None:
-            print("Sorry wrong format, please enter personal number as xxxxxx-xxxx")
+            print("\nSorry wrong format, please enter social security number as xxxxxx-xxxx")
         else:
             for x in self.customers:
                 if pnr == x.pnr:
                     for y in self.accounts:
                         if acc_no == y.accountno and y.id == x.id:
                             return f'{y.accountno}, {y.accounttype}, {y.balance}'
-                    return print(f"No account found with account number {acc_no}.")
-            return print(f"No customer found with {pnr} in list")
+                    return print(f"\nNo account found with account number {acc_no}.")
+            return print(f"\nNo customer found with {pnr} in list")
 
     def add_account(self, pnr):
         if re.match('[0-9]{6}-[0-9]{4}', pnr) is None:
-            print("Sorry wrong format, please enter personal number as xxxxxx-xxxx")
+            print("\nSorry wrong format, please enter social security number as xxxxxx-xxxx")
             return False
         else:
             for i in self.customer_data:
@@ -131,22 +107,21 @@ class Bank():
                     line = f'{i}#{Bank.get_top_account(self)}:debit account:0.0'
                     new_line = i.replace(i, line)
                     self.customer_data[index] = new_line
-
-        with open(self.ctxt, "w") as f:
-            f.writelines("%s\n" % line for line in self.customer_data)
-
-        print("\nNew account added!")
+                    with open(self.ctxt, "w") as f:
+                        f.writelines("%s\n" % line for line in self.customer_data)
+                    print(f'\nNew bank account with account number {line.replace("#", ":").split(":")[-3]} successfully added!')
+                    return True
 
     def add_customer(self, name, pnr):
 
         if re.match('[0-9]{6}-[0-9]{4}', pnr) is None:
-            print("Sorry wrong format, please enter personal number as xxxxxx-xxxx")
+            print("\nSorry wrong format, please enter social security number as xxxxxx-xxxx")
             return False
         elif any(pnr in s for s in self.customer_data):
-            print("Customer with same social security number already exists")
+            print("\nCustomer with same social security number already exists")
             return False
         else:
-            textfile = open("customers.txt","a")
+            textfile = open("customers.txt", "a")
             textfile.write('\n' + Bank.get_new_id(self) + f':{name}:{pnr}:' + Bank.get_top_account(self) + f':debit account:0.0')
             textfile.close()
             print("\nCustomer added!")
@@ -154,7 +129,7 @@ class Bank():
 
     def change_customer_name(self, newname, pnr):
         if re.match('[0-9]{6}-[0-9]{4}', pnr) is None:
-            print("\nSorry wrong format, please enter personal number as xxxxxx-xxxx")
+            print("\nSorry wrong format, please enter social security number as xxxxxx-xxxx")
             return False
         elif any(pnr in s for s in self.customer_data):
             for line in self.customer_data:
@@ -163,18 +138,21 @@ class Bank():
                     name = line.split(":")[1]
                     new_line = line.replace(name, newname)
                     self.customer_data[index] = new_line
-                    print(f'Name changed from {name} to {newname}')
+                    with open(self.ctxt, "w") as f:
+                        f.writelines("%s\n" % l for l in self.customer_data)
+                    print(f'\nName changed from {name} to {newname}')
+                    return True
 
         else:
             print(f'\nNo customer with {pnr} exists')
+            return False
 
-        with open(self.ctxt, "w") as f:
-            f.writelines("%s\n" % l for l in self.customer_data)
+
     
     def remove_customer(self, pnr):
 
         if re.match('[0-9]{6}-[0-9]{4}', pnr) is None:
-            print("\nSorry wrong format, please enter personal number as xxxxxx-xxxx")
+            print("\nSorry wrong format, please enter social security number as xxxxxx-xxxx")
             return False
         else:   
             for line in self.customer_data:
@@ -184,29 +162,23 @@ class Bank():
                     for i in self.customers:
                         if pnr == i.pnr:
                             acc = ""
-                            customer = f'Customer id: {i.id}\nName: {i.name}\nSocial security number: {i.pnr}\n'
+                            customer = f'\nCustomer id: {i.id}\nName: {i.name}\nSocial security number: {i.pnr}\n'
                             for x in self.accounts:
                                 if x.id == i.id:
                                     acc += f'\n\tAccount no: {x.accountno}\n\tAccount type: {x.accounttype}\n' \
                                            f'\tBalance returned: {x.balance}\n '
 
-                            print(f'{customer}\nAccounts closed: {acc}')
+                            print(f'\nCustomer successfully removed! \n{customer}\nAccounts closed: {acc}')
 
-        with open(self.ctxt, "w") as f:
-            f.writelines("%s\n" % l for l in self.customer_data)
+            with open(self.ctxt, "w") as f:
+                f.writelines("%s\n" % l for l in self.customer_data)
 
     def deposit(self, pnr, acc_no, amount):
-        account_data = {}
-        acc_no_list = []
         if re.match('[0-9]{6}-[0-9]{4}', pnr) is None:
-            print("Sorry wrong format, please enter personal number as xxxxxx-xxxx")
+            print("\nSorry wrong format, please enter social security number as xxxxxx-xxxx")
             return False
         else:
-            for i in self.customer_data:
-                x = i.replace("#", ":").split(":")
-                account_data[x[2]] = x[3:]
-
-            for rad in self.customer_data:
+           for rad in self.customer_data:
                 if pnr in repr(rad):
                     index = self.customer_data.index(rad)
                     r1 = rad.replace("#",":").split(":")
@@ -218,16 +190,16 @@ class Bank():
                     new_line = rad.replace(old_bal, str(new_bal))
                     self.customer_data[index] = new_line
                     print(f"\nDeposit successful! \nOld balance: {old_bal} \nNew balace: {new_bal}")
-            with open(self.ctxt, "w") as f:
-                f.writelines("%s\n" % l for l in self.customer_data)
+                    with open(self.ctxt, "w") as f:
+                        f.writelines("%s\n" % l for l in self.customer_data)
+                    return True
 
 
     def withdrawal(self, pnr, acc_no, amount):
         if re.match('[0-9]{6}-[0-9]{4}', pnr) is None:
-            print("Sorry wrong format, please enter personal number as xxxxxx-xxxx")
+            print("\nSorry wrong format, please enter social security number as xxxxxx-xxxx")
             return False
         else:
-            Bank._load(self)
             for rad in self.customer_data:
                 if pnr in repr(rad):
                     index = self.customer_data.index(rad)
@@ -242,21 +214,22 @@ class Bank():
                         return False
                     new_line = rad.replace(old_bal, str(new_bal))
                     self.customer_data[index] = new_line
-            print(f"\nWithdrawal successfull! \nOld balance: {old_bal} \nNew balace: {str(new_bal)}")
-            with open(self.ctxt, "w") as f:
-                f.writelines("%s\n" % l for l in self.customer_data)
+                    print(f"\nWithdrawal successfull! \nOld balance: {old_bal} \nNew balace: {str(new_bal)}")
+                    with open(self.ctxt, "w") as f:
+                        f.writelines("%s\n" % l for l in self.customer_data)
+                    return True
 
     def close_account(self, pnr, acc_no):
 
         if re.match('[0-9]{6}-[0-9]{4}', pnr) is None:
-            print("Sorry wrong format, please enter personal number as xxxxxx-xxxx")
+            print("\nSorry wrong format, please enter social security number as xxxxxx-xxxx")
             return False
         else:
             for line in self.customer_data:
                 if pnr and acc_no in line:
                     index = self.customer_data.index(line)
                     if line.count("#") == 0:
-                        print("Account removed. Customer has no more accounts and has therefor also been removed. Details below:\n")
+                        print("\nAccount removed. Customer has no more accounts and has therefor also been removed. Details below:\n")
                         Bank.remove_customer(self, pnr)
                         with open(self.ctxt, "w") as f:
                             f.writelines("%s\n" % l for l in self.customer_data)
@@ -264,7 +237,7 @@ class Bank():
                         acc = Bank.get_account(self, pnr, acc_no).replace(", ", ":")
                         new_line = line.replace("#" + acc, "").replace(acc + "#", "")
                         self.customer_data[index] = new_line
-                        print(f'Account with account number {acc.split(":")[0]} closed.\nType: {acc.split(":")[1]}\nBalance returned: {acc.split(":")[2]}')
+                        print(f'\nAccount with account number {acc.split(":")[0]} closed.\nType: {acc.split(":")[1]}\nBalance returned: {acc.split(":")[2]}')
                         with open(self.ctxt, "w") as f:
                             f.writelines("%s\n" % l for l in self.customer_data)
 
@@ -286,7 +259,3 @@ class Bank():
         newAccount = int(max(self.accountNo)) + 1
 
         return str(newAccount)
-
-m = Bank()
-m.withdrawal("990383-3555","101","100")
-#m.close_account("139741-6173", "1002")
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from Account import Account
-from Customer import Customer
+#from Account import Account
+#from Customer import Customer
 from Bank import Bank
 
 mybank = Bank()
@@ -8,22 +8,30 @@ mybank = Bank()
 inp = True
 while inp:
     print("\nPlease choose an action from the Menu:\n")
-    print("1. Print list of customers, Name and pnr \n2. Add new customer \n3. Change customer name \n4. Delete customer \n5. Delete account \n6. See customer information (accountnumbers, balance, account types) \n7. Deposit money \n8. Withdraw money \n9. Add account \n10. Get account info\n11. Leave the bank\n")
+    print("1. Print list of customers, Name and pnr "
+          "\n2. Add new customer "
+          "\n3. Change customer name "
+          "\n4. Delete customer "
+          "\n5. Delete account "
+          "\n6. See customer information (account numbers, balance, account types) "
+          "\n7. Deposit money "
+          "\n8. Withdraw money "
+          "\n9. Add bank account "
+          "\n10. Get account info "
+          "\n11. Leave the bank\n")
 
     inp = input()
 
     if inp == "1":
         mybank.get_customers()
     elif inp == "2":
-        name = input("Enter customers full name: ")
+        name = input("Enter customers first and last name: ")
         pnr = input("Enter customers social security number: ")
         mybank.add_customer(name,pnr)
-        #mybank.add_customer("Kalle Anka","900101-1234")
     elif inp == "3":
-        newname = input("Enter new customer name: ")
+        newname = input("Enter new customer name, first and last name: ")
         pnr = input("Enter customers social security number: ")
         mybank.change_customer_name(newname,pnr)
-        #mybank.change_customer_name("Nalle Puh", "900101-1234")
     elif inp == "4":
         pnr = input("Enter customers social security number: ")
         mybank.remove_customer(pnr)
@@ -31,7 +39,6 @@ while inp:
         pnr = input("Enter customers social security number: ")
         acc_no = input("Enter account number you wish to close: ")
         mybank.close_account(pnr, acc_no)
-        #mybank.close_account("990383-3555", "1001")
     elif inp == "6":
         pnr = input("Enter customers social security number: ")
         mybank.get_customer(pnr)
@@ -40,13 +47,11 @@ while inp:
         acc_no = input("Enter account number to which you would like to deposit: ")
         amount = input("Enter amount you would like to deposit: ")
         mybank.deposit(pnr, acc_no, amount)
-        #mybank.deposit("990383-3555", "1001", "222")
     elif inp == "8":
         pnr = input("Enter customers social security number: ")
         acc_no = input("Enter account number from which you would like to withdraw: ")
         amount = input("Enter amount you would like to withdraw: ")
         mybank.withdrawal(pnr, acc_no, amount)
-        #mybank.withdrawal("990383-3555","1001", "222")
     elif inp == "9":
         pnr = input("Enter customers social security number: ")
         mybank.add_account(pnr)
